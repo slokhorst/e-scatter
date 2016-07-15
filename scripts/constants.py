b98582b719469eb68a06de2a69b968aa3aec3b7a
Johan Hidding <j.hidding@esciencecenter.nl>
2016-07-15 13:41:37 +0200

    refactor Phonon script

---
+++ scripts/constants.py
@@ -0,0 +1,12 @@
+from math import pi
+
+
+h = 6.626070040e-34         # Planck
+h_bar = h/(2*pi)            # Planck bar
+k_B = 1.3806488e-23         # Boltzmann
+m_e = 9.10938215e-31        # electron mass
+q_e = 1.602176565e-19       # electron charge
+eV = q_e                    # electron-volt
+N_A = 6.022141e23           # Avogrado
+T = 300                     # room temperature
+
