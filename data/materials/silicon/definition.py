ac96d075269da97a435f12c6dc58d5283e987c68
Johan Hidding <j.hidding@esciencecenter.nl>
2016-07-20 13:35:48 +0200

    added some data

---
+++ data/materials/silicon/definition.py
@@ -0,0 +1,17 @@
+{
+	'name': 'silicon',
+
+	'rho_m': 2.3290e3,
+	'fermi': 7.83*eV,
+	'work_func': 4.79*eV,
+	'band_gap': 1.12*eV,
+	'lattice': 0.543e-9,
+	'c_s': 9040,
+	'eps_ac': 9.2*eV,
+
+	'inelastic_xml_file': 'inelastic-silicon.xml',
+
+	'elements': {
+		'Si': {'count': 1, 'Z': 14, 'M': 28.085e-3}
+	}
+}
---
+++ data/materials/silicondioxide/definition.py
@@ -0,0 +1,18 @@
+{
+	'name': 'silicondioxide',
+
+	'rho_m': 2.648e3,
+	'fermi': 0*eV,
+	'work_func': 0.9*eV,
+	'band_gap': 9*eV,
+	'lattice': 0.545e-9,
+	'c_s': 3560,
+	'eps_ac': 2.6*eV,
+
+	'inelastic_xml_file': 'inelastic-silicondioxide.xml',
+
+	'elements': {
+		'Si': {'count': 1, 'Z': 14, 'M': 28.085e-3},
+		'O':  {'count': 2, 'Z': 8, 'M': 15.999e-3}
+	}
+}
