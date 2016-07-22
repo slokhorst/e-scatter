c1cc4142598447f6baf7e0141838d1cd0c95cfb1
Sebastiaan Lokhorst <sebastiaanlokhorst@gmail.com>
2016-07-22 16:12:17 +0200

    Add siliconnitride material

---
+++ data/materials/siliconnitride/definition.py
@@ -0,0 +1,18 @@
+{
+	'name': 'siliconnitride',
+
+	'rho_m': 3.2e3,
+	'fermi': 0*eV,
+	'work_func': 1.5*eV,
+	'band_gap': 4.5*eV,
+	'lattice': 0.545e-9,
+	'c_s': 3560,
+	'eps_ac': 2.6*eV,
+
+	'elf-file': 'data/elf/df_Si3N4.dat',
+
+	'elements': {
+		'Si': {'count': 3, 'Z': 14, 'M': 28.085e-3},
+		'N':  {'count': 4, 'Z': 7, 'M': 14.007e-3}
+	}
+}
