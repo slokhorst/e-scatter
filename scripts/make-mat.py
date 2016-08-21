98c62d950b8ca28ec8bdf26a8cd17869b866ee8b
Sebastiaan Lokhorst <s.r.lokhorst@student.tudelft.nl>
2016-08-21 14:58:34 +0200

    Improve make-mat error checking and status messages

--- scripts/make-mat.py
+++ scripts/make-mat.py
@@ -69,30 +69,30 @@ def run_elsepa(Z, out_fn):
         # kinetic energy (eV)                       [none]
         elscata_in += 'EV      {}\n'.format(E)
 
-    p = subprocess.run(
+    subprocess.run(
         [os.path.join(elsepa_dir, 'elscata')],
-        input=bytes(elscata_in, 'UTF-8'), stdout=subprocess.DEVNULL)
-    p.check_returncode()
+        input=bytes(elscata_in, 'UTF-8'), stdout=subprocess.DEVNULL, check=True)
 
     os.chdir(work_dir)
 
-    p = subprocess.run(["scripts/elsepa-parse.py", elsepa_dir], stdout=out_xml)
+    subprocess.run(
+        ["scripts/elsepa-parse.py", elsepa_dir],
+        stdout=out_xml, check=True)
 
 
 def run_phonon(eps_ac, c_s, rho_m, M_tot, lattice, out_fn):
     out_xml = open(out_fn, 'w')
-    p = subprocess.run(
+    subprocess.run(
         ['scripts/phonon-gen.py', '--eps_ac', str(eps_ac), '--c_s', str(c_s),
          '--rho_m', str(rho_m), '--M', str(M_tot), '--a', str(lattice)],
-        stdout=out_xml)
-    p.check_returncode()
+        stdout=out_xml, check=True)
 
 
 def run_endf(Z, out_fn):
     out_xml = open(out_fn, 'w')
-    p = subprocess.run(
-        ["scripts/endf-parse.py", endf_dir, str(Z)], stdout=out_xml)
-    p.check_returncode()
+    subprocess.run(
+        ['scripts/endf-parse.py', endf_dir, str(Z)],
+        stdout=out_xml, check=True)
 
 ###
 
@@ -101,73 +101,71 @@ el_mott_fn = os.path.join(mat_dir, 'elastic-mott.xml')
 el_fn = os.path.join(mat_dir, 'elastic.xml')
 inel_fn = os.path.join(mat_dir, 'inelastic.xml')
 ion_fn = os.path.join(mat_dir, 'ionization.xml')
-outer_shell_fn = os.path.join(mat_dir, 'outer_shell.dat')
-
-el_xml = open(el_fn, 'w')
-inel_xml = open(inel_fn, 'w')
-ion_xml = open(ion_fn, 'w')
+osi_fn = os.path.join(mat_dir, 'outer_shell.dat')
 
 with open(el_mott_fn, 'w') as el_mott_xml:
     el_mott_xml.write('<cstable type="elastic">\n</cstable>\n')
 
-i = 0
 for elem in mat['elements']:
-    i += 1
     Z = mat['elements'][elem]['Z']
     count = mat['elements'][elem]['count']
 
     element_el_mott_fn = os.path.join(
-        mat_dir, "elastic-mott-{}.xml".format(elem))
+        mat_dir, 'elastic-mott-{}.xml'.format(elem))
+    print('running Elsepa for element {}'.format(elem), file=sys.stderr)
     run_elsepa(Z, element_el_mott_fn)
     with open('tmp.xml', 'w') as tmp_xml:
-        p = subprocess.run(
+        subprocess.run(
             ['bin/cstool', 'mad', str(1), el_mott_fn, str(count),
-             element_el_mott_fn], stdout=tmp_xml)
-        p.check_returncode()
-
+             element_el_mott_fn],
+             stdout=tmp_xml, check=True)
     os.rename('tmp.xml', el_mott_fn)
 
-    element_ion_fn = os.path.join(mat_dir, "ionization-{}.xml".format(elem))
+    element_ion_fn = os.path.join(mat_dir, 'ionization-{}.xml'.format(elem))
+    print('parsing ENDF files for element {}'.format(elem), file=sys.stderr)
     run_endf(Z, element_ion_fn)
-    p = subprocess.run(
-        ['bin/cstool', 'mad', str(count), element_ion_fn], stdout=ion_xml)
-    p.check_returncode()
+    with open(ion_fn, 'w') as ion_xml:
+        subprocess.run(
+            ['bin/cstool', 'mad', str(count), element_ion_fn],
+            stdout=ion_xml, check=True)
 
-run_phonon(
-    mat['eps_ac'], mat['c_s'], mat['rho_m'], mat['M_tot'], mat['lattice'],
-    el_phon_fn)
+print('generating phonon cross-sections', file=sys.stderr)
+run_phonon(mat['eps_ac'], mat['c_s'], mat['rho_m'],
+    mat['M_tot'], mat['lattice'], el_phon_fn)
 
-p = subprocess.run(
-    ['bin/cstool', 'merge', el_phon_fn, el_mott_fn, '100*eV', '200*eV'],
-    stdout=el_xml)
-p.check_returncode()
+with open(el_fn, 'w') as el_xml:
+    subprocess.run(
+        ['bin/cstool', 'merge', el_phon_fn, el_mott_fn, '100*eV', '200*eV'],
+        stdout=el_xml, check=True)
 
 with open('tmp.xml', 'w') as tmp_xml:
-    p = subprocess.run(
-        ['bin/cstool', 'shift', el_fn, str(mat['fermi'])], stdout=tmp_xml)
-    p.check_returncode()
+    subprocess.run(
+        ['bin/cstool', 'shift', el_fn, str(mat['fermi'])],
+        stdout=tmp_xml, check=True)
+os.rename('tmp.xml', el_fn)
 
-with open(mat['elf-file'], 'r') as elf_f, open(outer_shell_fn, 'w') as osi_f:
+with open(mat['elf-file'], 'r') as elf_f, open(osi_fn, 'w') as osi_f:
     osi_f.write(elf_f.readline())
 
-os.rename('tmp.xml', el_fn)
-p = subprocess.run(
-    ['scripts/inelastic-gen.py', '--number-density', str(mat['rho_n']),
-     '--elf-file', mat['elf-file'], '--fermi',
-     str(mat['fermi'] / eV)], stdout=inel_xml)
-p.check_returncode()
+print('generating inelastic cross-sections', file=sys.stderr)
+with open(inel_fn, 'w') as inel_xml:
+    subprocess.run(
+        ['scripts/inelastic-gen.py', '--number-density', str(mat['rho_n']),
+         '--elf-file', mat['elf-file'], '--fermi', str(mat['fermi'] / eV)],
+        stdout=inel_xml, check=True)
 
-p = subprocess.run(
+print('compiling material', file=sys.stderr)
+subprocess.run(
     ['bin/cstool', 'compile-mat',
      '{}/{}.mat'.format(mat_dir, mat['name']),
      '--name', mat['name'],
      '--elastic', el_fn,
      '--inelastic', inel_fn,
      '--ionization', ion_fn,
-     '--outer-shell', outer_shell_fn,
+     '--outer-shell', osi_fn,
      '--number-density', str(mat['rho_n']),
      '--fermi-energy', str(mat['fermi']),
      '--work-function', str(mat['work_func']),
      '--band-gap', str(mat['band_gap']),
-     '--phonon-loss', str(mat['phonon_loss'])])
-p.check_returncode()
+     '--phonon-loss', str(mat['phonon_loss'])],
+    check=True)
