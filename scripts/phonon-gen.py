slokhorst/e-scatter

commit 29256444fb40b8d3b8a533bdaebe3997a2a56d75

Added reduced wavevector correction with respect to band edge

scripts/phonon-gen.py
@@ -24,8 +24,8 @@ def interpolate(f1, f2, h, a, b):
     """Interpolate two functions `f1` and `f2` using interpolation
     function `h`, which maps [0,1] to [0,1] one-to-one."""
     def g(x):
-        y1 = f1(x)
-        y2 = f2(x)
+        y1 = f1(a)
+        y2 = f2(b)
         u = (x - a) / (b - a)
         w = h(u)
         ym = (1 - w) * y1 + w * y2

scripts/constants.py
@@ -11,3 +11,5 @@ eV = q_e                    # electron-volt [J]
 N_A = 6.022141e23           # Avogrado [#]
 T = 300                     # room temperature [K]
 a_0 = 5.2917721067e-11      # Bohr radius [m]
+c = 299792458               # speed of light [m/s]
+mc2 = m_e*(c**2)

scripts/elsepa-parse.py
@@ -58,10 +58,10 @@ def parse_folder(dirname):
         if not os.path.isfile(dcs_fpath):
             raise FileNotFoundError(dcs_fpath)
 
-        print('\t<cross-section energy="{}*eV"><!--tcs="{}*m^2"-->'
+        print('\t<cross-section energy="{:.16e}*eV"><!--tcs="{:.16e}*m^2"-->'
               .format(energy, tcs))
         for angle, dcs in sorted(parse_dcs_dat(dcs_fpath).items()):
-            print('\t\t<insert angle="{}*pi/180" dcs="{}*m^2/sr" />'
+            print('\t\t<insert angle="{:.16e}*pi/180" dcs="{:.16e}*m^2/sr" />'
                   .format(angle, dcs))
         print('\t</cross-section>')
     print('</cstable>\n')

scripts/inelastic-gen.py
@@ -4,9 +4,9 @@
 # Use on KieftBosch optical ELF data (data/elf/*.dat)
 import argparse
 import numpy as np
-from numpy import (log, sqrt, log10, pi)
+from numpy import (exp, log, sqrt, log10, pi)
 import scipy.interpolate
-from constants import (q_e, a_0)
+from constants import (q_e, a_0, mc2, eV)
 
 
 def getdata(elf_file):
@@ -19,7 +19,7 @@ def getdata(elf_file):
                 continue
 
             w0 = float(vals[0])     # in eV
-            elf = float(vals[1])    # in ?
+            elf = float(vals[1])    # dimensionless
 
             if w0 < 0 or elf < 0:
                 continue
@@ -32,11 +32,12 @@ def getdata(elf_file):
 
 def L_Kieft(w0, K, F):
     a = w0 / K
-    x1 = 2/a * (1 + sqrt(1 - 2*a)) - 1
-    x2 = (K - F - w0) / (K - F + w0)
-    L1 = log(x1) + log(x2)
+    x1 = 2.0/a*(1.0 + sqrt(1.0 - 2.0*a)) - 1.0
+    x2 = K-F-w0
+    x3 = K-F+w0
+    L1 = 1.5*(log(x1)+log(x2)-log(x3));
     L2 = -log(a)
-    return (w0 / K < 1/2) * 1.5*L1 + (w0 > 50) * L2
+    return max(0, (a<0.5)*(w0<50)*1.5*L1 + (w0>50)*L2)
 
 
 def L_Ashley_w_ex(w0, K, _):
@@ -76,15 +77,20 @@ F = args.fermi
 rho_n = args.number_density
 L = methods[args.L]
 w0, elf = getdata(args.elf_file)
-elf_interp = scipy.interpolate.PchipInterpolator(w0, elf)
+elf_interp = scipy.interpolate.PchipInterpolator(log(w0), log(elf))
 
-K_bounds = (max(2*F,1),1e6)
+K_bounds = (F+0.1,10e3)
 
 print('<cstable type="inelastic">')
 for K in np.logspace(log10(K_bounds[0]), log10(K_bounds[1]), num=1024):
     print('<cross-section energy="{}*eV">'.format(K))
-    for w in np.logspace(log10(w0[0]), log10(K/2 - 0.1), num=1024):
-        dcs = elf_interp(w)*L(w, K, F) / (2*pi*K*a_0*q_e * rho_n)
-        print('\t<insert omega0="{}*eV" dcs="{}*m^2" />'.format(w, dcs))
+    w0_max = K/2;
+    if L == L_Kieft:
+        w0_max -= F/2
+        #w0_max *= 2
+    for w in np.logspace(log10(w0[0]), log10(w0_max), num=1024):
+        dcs = exp(elf_interp(log(w)))*L(w, K, F) / (2*pi*a_0*q_e*rho_n)
+        dcs /= 0.5*(1.0-1.0/(K/(mc2/eV)+1)**2)*(mc2/eV)
+        print('\t<insert omega0="{:.16e}*eV" dcs="{:.16e}*m^2" />'.format(w, dcs))
     print('</cross-section>')
 print('</cstable>')

scripts/make-mat.py
@@ -21,7 +21,7 @@ def run_elsepa(Z, out_fn):
 
     mexch = 1
     mcpol = 2
-    mabs = 0
+    mabs = 1
     muffin = 1
     if Z in no_muffin_Z:
         muffin = 0
@@ -49,8 +49,7 @@ def run_elsepa(Z, out_fn):
     for E in [10, 20, 30, 40, 50, 60, 70, 80, 90,
               100, 200, 300, 400, 500, 600, 700, 800, 900,
               1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000,
-              10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000,
-              100000]:
+              10000]:
         # kinetic energy (eV)                       [none]
         elscata_in += 'EV      {}\n'.format(E)
 
@@ -148,12 +147,6 @@ with open(el_fn, 'w') as el_xml:
     subprocess.run(
         ['bin/cstool', 'merge', el_phon_fn, el_mott_fn, '100*eV', '200*eV'],
         stdout=el_xml, check=True)
-
-with open('tmp.xml', 'w') as tmp_xml:
-    subprocess.run(
-        ['bin/cstool', 'shift', el_fn, str(mat['fermi'])],
-        stdout=tmp_xml, check=True)
-os.rename('tmp.xml', el_fn)
 
 with open(mat['elf-file'], 'r') as elf_f, open(osi_fn, 'w') as osi_f:
     osi_f.write(elf_f.readline())

src/cdsem/cuda_kernels.cu
@@ -12,10 +12,10 @@
 
 __device__ const float _eps = 10.0f*FLT_EPSILON;
 __device__ const float _me = 9.10938356e-31f; // electron mass [kg]
-__device__ const float _c2 = 8.98755179e+16f; // speed of light squared [m/s]
-__device__ const float _eV = 1.60217662e-19f; // one electron volt [C]
+//__device__ const float _c2 = 8.98755179e+16f; // speed of light squared [m/s]
+//__device__ const float _eV = 1.60217662e-19f; // one electron volt [C]
 __device__ const float _NA = 6.02214086e+23f; // Avogadro's constant [/mol]
-__device__ const float _mc2 = _me*_c2/_eV;    // rest mass of electron [eV]
+//__device__ const float _mc2 = _me*_c2/_eV;    // rest mass of electron [eV]
 __device__ const float _pi = 3.14159265f;
 
 __device__ float posf(float x) {
@@ -77,7 +77,7 @@ __device__ T interp2(const T* ptr, int pitch, int height, int3 offset, int2 dim,
 
 __device__ float3 make_unit_vec(float cos_theta, float phi) {
     float sin_phi, cos_phi;
-    __sincosf(phi, &sin_phi, &cos_phi);
+    sincosf(phi, &sin_phi, &cos_phi);
     cos_theta = clamp(cos_theta, -1.0f, 1.0f);
     const float sin_theta = sqrtf(1.0f-cos_theta*cos_theta);
     return make_float3(sin_theta*cos_phi, sin_theta*sin_phi, cos_theta);
@@ -89,7 +89,7 @@ __device__ float3 make_unit_vec(float cos_theta, float phi) {
  */
 __device__ float3 make_normal_vec(float3 dir, float phi) {
     float sin_azimuth, cos_azimuth;
-    __sincosf(atan2f(dir.y, dir.x), &sin_azimuth, &cos_azimuth);
+    sincosf(atan2f(dir.y, dir.x), &sin_azimuth, &cos_azimuth);
 
     const float3 unit_v = make_float3(
         dir.z*cos_azimuth,
@@ -99,7 +99,7 @@ __device__ float3 make_normal_vec(float3 dir, float phi) {
     const float3 unit_u = cross_product(unit_v, dir);
 
     float sin_phi, cos_phi;
-    __sincosf(phi, &sin_phi, &cos_phi);
+    sincosf(phi, &sin_phi, &cos_phi);
     return unit_u*cos_phi+unit_v*sin_phi;
 }
 
@@ -215,17 +215,17 @@ __global__ void cuda_init_trajectory(cuda_particle_struct pstruct, cuda_geometry
 
     float elastic_imfp, inelastic_imfp, total_imfp;
     {// log-log interpolate the elastic and inelastic inverse mean free path
-        const float x = __logf(K/mstruct.K_min)/__logf(mstruct.K_max/mstruct.K_min);
+        const float x = logf(K/mstruct.K_min)/logf(mstruct.K_max/mstruct.K_min);
         const int3 offset = make_int3(0, 0, material_idx);
         const int dim = mstruct.K_cnt;
-        elastic_imfp = __expf(interp1(mstruct.elastic_dev_p, mstruct.pitch, mstruct.P_cnt+1, offset, dim, x));
-        inelastic_imfp = __expf(interp1(mstruct.inelastic_dev_p, mstruct.pitch, mstruct.P_cnt+1, offset, dim, x));
+        elastic_imfp = expf(interp1(mstruct.elastic_dev_p, mstruct.pitch, mstruct.P_cnt+1, offset, dim, x));
+        inelastic_imfp = expf(interp1(mstruct.inelastic_dev_p, mstruct.pitch, mstruct.P_cnt+1, offset, dim, x));
         total_imfp = elastic_imfp+inelastic_imfp;
     }
 
     curand_wrapper curand(rand_state_dev_p[particle_idx]);
 
-    pstruct.distance_dev_p[particle_idx] = -__logf(curand.uniform())/total_imfp;
+    pstruct.distance_dev_p[particle_idx] = -logf(curand.uniform())/total_imfp;
     if(curand.uniform() < elastic_imfp/total_imfp)
         pstruct.status_dev_p[particle_idx] = cuda_particle_struct::ELASTIC_EVENT;
     else
@@ -447,6 +447,7 @@ __global__ void cuda_intersection_event(cuda_particle_struct pstruct, cuda_geome
     float3 normal = cross_product(e1, e2);
     normal *= rnorm3df(normal.x, normal.y, normal.z);
     const float cos_theta = dot_product(normal, dir);
+    const float sin_theta = sqrtf(1.0f-__saturatef(cos_theta*cos_theta));
 
     int material_idx_in, material_idx_out;
     if(cos_theta > 0) {
@@ -476,18 +477,25 @@ __global__ void cuda_intersection_event(cuda_particle_struct pstruct, cuda_geome
     }
 
     float dU = 0;
-    if(material_idx_out >= 0)
-        dU += mstruct.barrier_dev_p[material_idx_out];
-    if(material_idx_in >= 0)
+    float band_edge[2] = {0.0f, 0.0f};
+    float effective_mass[2] = {1.0f, 1.0f};
+    if(material_idx_in >= 0) {
         dU -= mstruct.barrier_dev_p[material_idx_in];
+        band_edge[0] = mstruct.band_edge_dev_p[material_idx_in];
+        effective_mass[0] = mstruct.effective_mass_dev_p[material_idx_in];
+    }
+    if(material_idx_out >= 0) {
+        dU += mstruct.barrier_dev_p[material_idx_out];
+        band_edge[1] = mstruct.band_edge_dev_p[material_idx_out];
+        effective_mass[1] = mstruct.effective_mass_dev_p[material_idx_out];
+    }
 
     curand_wrapper curand(rand_state_dev_p[particle_idx]);
 
-    // R. Shimizu and Z. J. Ding, Rep. Prog. Phys., 55, 487-531, 1992
-    //  see equations 3.20, 3.23 and 3.24
     const float K = pstruct.K_energy_dev_p[particle_idx];
     if(K*cos_theta*cos_theta+dU > 0) {
-        const float s = sqrtf(1.0f+dU/(K*cos_theta*cos_theta));
+        const float r = effective_mass[1]/effective_mass[0];
+        const float s = sqrtf(r*(K+dU-band_edge[1])/(K-band_edge[0])-sin_theta*sin_theta)/fabsf(cos_theta);
         const float T = (opt.quantum_transmission_flag) ? 4.0f*s/((1.0f+s)*(1.0f+s)) : 1.0f;
         if(curand.uniform() < T) {
             if(opt.interface_refraction_flag) {
@@ -497,16 +505,18 @@ __global__ void cuda_intersection_event(cuda_particle_struct pstruct, cuda_geome
             }
             pstruct.K_energy_dev_p[particle_idx] = K+dU;
             pstruct.material_idx_dev_p[particle_idx] = material_idx_out;
+            return;
         }
-    } else if((opt.interface_absorption_flag) && (dU < 0) && (curand.uniform() < __expf(1.0f+0.5f*K/dU))) {
+    }
+    if((opt.interface_absorption_flag) && (dU < 0) && (curand.uniform() < expf(1.0f+0.5f*K/dU))) {
         // surface absorption? (this is in accordance with Kieft & Bosch code)
         pstruct.status_dev_p[particle_idx] = cuda_particle_struct::TERMINATED;
-    } else {
-        // total internal reflection
-        pstruct.dir_x_dev_p[particle_idx] = dir.x-2.0f*normal.x*cos_theta;
-        pstruct.dir_y_dev_p[particle_idx] = dir.y-2.0f*normal.y*cos_theta;
-        pstruct.dir_z_dev_p[particle_idx] = dir.z-2.0f*normal.z*cos_theta;
+        return;
     }
+    // total internal reflection
+    pstruct.dir_x_dev_p[particle_idx] = dir.x-2.0f*normal.x*cos_theta;
+    pstruct.dir_y_dev_p[particle_idx] = dir.y-2.0f*normal.y*cos_theta;
+    pstruct.dir_z_dev_p[particle_idx] = dir.z-2.0f*normal.z*cos_theta;
 }
 
 __global__ void cuda_elastic_event(cuda_particle_struct pstruct, cuda_material_struct mstruct, curandState* rand_state_dev_p, scatter_options opt) {
@@ -532,7 +542,7 @@ __global__ void cuda_elastic_event(cuda_particle_struct pstruct, cuda_material_s
 
     float cos_theta, sin_theta;
     {// sample random elastic scatter angle
-        const float x = __logf(K/mstruct.K_min)/__logf(mstruct.K_max/mstruct.K_min);
+        const float x = logf(K/mstruct.K_min)/logf(mstruct.K_max/mstruct.K_min);
         const float y = curand.uniform();
         const int3 offset = make_int3(0, 1, material_idx);
         const int2 dim = make_int2(mstruct.K_cnt, mstruct.P_cnt);
@@ -555,14 +565,13 @@ __global__ void cuda_elastic_event(cuda_particle_struct pstruct, cuda_material_s
     pstruct.dir_y_dev_p[particle_idx] = dir.y*cos_theta+normal_dir.y*sin_theta;
     pstruct.dir_z_dev_p[particle_idx] = dir.z*cos_theta+normal_dir.z*sin_theta;
 
-    if(K < 300) {
+    if(K < 200) {
         if(opt.acoustic_phonon_loss_flag)
-            pstruct.K_energy_dev_p[particle_idx] = K-fminf(50e-3f, mstruct.phononloss_dev_p[material_idx]);
-            //pstruct.K_energy_dev_p[particle_idx] = K-fminf(50e-3f, 6.563905512e-3f);
+            pstruct.K_energy_dev_p[particle_idx] = K-fminf(50e-3f, mstruct.phonon_loss_dev_p[material_idx]);
     } else {
         if(opt.atomic_recoil_loss_flag) {
             #warning "fixed energy loss due to atom recoil assumes silicon material"
-            pstruct.K_energy_dev_p[particle_idx] = K-2.0f*(_me*_NA)*K*(1.0f-cos_theta)/28.1e-3;
+            pstruct.K_energy_dev_p[particle_idx] = K-2.0f*(_me*_NA)*K*(1.0f-cos_theta)/28.1f;
         }
     }
 }
@@ -599,17 +608,17 @@ __global__ void cuda_inelastic_event(cuda_particle_struct pstruct, cuda_material
 
     float omega0;
     {// sample random zero-momentum energy loss of the primary electron
-        const float x = __logf(K/mstruct.K_min)/__logf(mstruct.K_max/mstruct.K_min);
+        const float x = logf(K/mstruct.K_min)/logf(mstruct.K_max/mstruct.K_min);
         const float y = curand.uniform();
         const int3 offset = make_int3(0, 1, material_idx);
         const int2 dim = make_int2(mstruct.K_cnt, mstruct.P_cnt);
-        omega0 = __expf(interp2(mstruct.inelastic_dev_p, mstruct.pitch, mstruct.P_cnt+1, offset, dim, x, y));
-        omega0 = clamp(omega0, 0.0f, K-fermi);
+        omega0 = expf(interp2(mstruct.inelastic_dev_p, mstruct.pitch, mstruct.P_cnt+1, offset, dim, x, y));
+        omega0 = posf(omega0);
     }
 
     float binding;
     {// sample random binding energy of the secondary electron
-        const float x = __logf(omega0/mstruct.K_min)/__logf(mstruct.K_max/mstruct.K_min);
+        const float x = logf(omega0/mstruct.K_min)/logf(mstruct.K_max/mstruct.K_min);
         const float y = curand.uniform();
         const int ix = clamp(__float2int_rd(x*(mstruct.K_cnt-1)), 0, mstruct.K_cnt-1);
         const int iy = clamp(__float2int_rd(y*(mstruct.P_cnt-1)), 0, mstruct.P_cnt-1);
@@ -633,8 +642,8 @@ __global__ void cuda_inelastic_event(cuda_particle_struct pstruct, cuda_material
             // binding energy for omegaprime (so that the differential cross section becomes inversely
             // proportional to both the total energy transfer and the kinetic energy of the secondary
             // electron).
-            const float f_min = 1.0f/w0*__logf((omega_min-w0)/omega_min);
-            const float f_max = 1.0f/w0*__logf((omega_max-w0)/omega_max);
+            const float f_min = 1.0f/w0*logf((omega_min-w0)/omega_min);
+            const float f_max = 1.0f/w0*logf((omega_max-w0)/omega_max);
             omega = -w0/expm1f(w0*(f_min*(1.0f-U)+f_max*U));
         } else {
             // In some cases (typically only occuring for binding < 50 eV) we get omega_min > omega_max.
@@ -648,15 +657,15 @@ __global__ void cuda_inelastic_event(cuda_particle_struct pstruct, cuda_material
     }
 
     if(binding < 0) {
-        const float bandgap = mstruct.bandgap_dev_p[material_idx];
-        if(bandgap < 0) {
+        const float band_gap = mstruct.band_gap_dev_p[material_idx];
+        if(band_gap < 0) {
             // metal: excitation of a fermi sea electron
             // TODO
-        } else if(omega0 > bandgap) {
-            // electron excitation across the bandgap
-            binding = bandgap;
+        } else if(omega0 > band_gap) {
+            // electron excitation across the band gap
+            binding = band_gap;
         } else {
-            // sub-bandgap energy loss in semiconductors and insulators
+            // sub-band gap energy loss in semiconductors and insulators
             // energy loss due to longitudinal optical phonon excitation is assumed
             if(opt.optical_phonon_loss_flag)
                 pstruct.K_energy_dev_p[primary_idx] = K-omega0;
@@ -664,9 +673,6 @@ __global__ void cuda_inelastic_event(cuda_particle_struct pstruct, cuda_material
         }
     }
     binding = posf(binding);
-
-    if(omega <= 0)
-        return;
 
     float3 primary_dir = make_float3(
         pstruct.dir_x_dev_p[primary_idx],
@@ -680,8 +686,8 @@ __global__ void cuda_inelastic_event(cuda_particle_struct pstruct, cuda_material
 
     const float _K = K-fermi+2.0f*binding;
     const float dK = binding+omega;
-    const float cos_theta = sqrtf(__saturatef(dK/_K));
-    const float sin_theta = sqrtf(1.0f-cos_theta*cos_theta);
+    const float cos_theta = sqrtf(dK/_K);
+    const float sin_theta = sqrtf(1.0f-__saturatef(cos_theta*cos_theta));
 
     float3 secondary_dir = make_float3(
         primary_dir.x*cos_theta+normal_dir.x*sin_theta,
@@ -691,7 +697,8 @@ __global__ void cuda_inelastic_event(cuda_particle_struct pstruct, cuda_material
     if(opt.instantaneous_momentum_flag) {
         const float random_cos = 2.0f*curand.uniform()-1.0f;
         const float random_phi = 2.0f*_pi*curand.uniform();
-        secondary_dir += sqrtf(posf(binding/dK))*make_unit_vec(random_cos, random_phi);
+        secondary_dir *= rnorm3df(secondary_dir.x, secondary_dir.y, secondary_dir.z);
+        secondary_dir += sqrtf(binding/dK)*make_unit_vec(random_cos, random_phi);
     }
     secondary_dir *= rnorm3df(secondary_dir.x, secondary_dir.y, secondary_dir.z);
 

src/cdsem/cuda_material_struct.cu
@@ -17,8 +17,10 @@ __host__ cuda_material_struct cuda_material_struct::create(int capacity) {
         size_t _pitch;
         cudaMalloc(&mstruct.fermi_dev_p, capacity*sizeof(float));
         cudaMalloc(&mstruct.barrier_dev_p, capacity*sizeof(float));
-        cudaMalloc(&mstruct.bandgap_dev_p, capacity*sizeof(float));
-        cudaMalloc(&mstruct.phononloss_dev_p, capacity*sizeof(float));
+        cudaMalloc(&mstruct.band_gap_dev_p, capacity*sizeof(float));
+        cudaMalloc(&mstruct.band_edge_dev_p, capacity*sizeof(float));
+        cudaMalloc(&mstruct.effective_mass_dev_p, capacity*sizeof(float));
+        cudaMalloc(&mstruct.phonon_loss_dev_p, capacity*sizeof(float));
         cudaMallocPitch(&mstruct.elastic_dev_p, &_pitch, mstruct.K_cnt*sizeof(float), (mstruct.P_cnt+1)*capacity);
         cudaMallocPitch(&mstruct.inelastic_dev_p, &_pitch, mstruct.K_cnt*sizeof(float), (mstruct.P_cnt+1)*capacity);
         cudaMallocPitch(&mstruct.ionization_dev_p, &_pitch, mstruct.K_cnt*sizeof(float), (mstruct.P_cnt+1)*capacity);
@@ -31,8 +33,10 @@ __host__ void cuda_material_struct::release(cuda_material_struct& mstruct) {
     cuda_safe_call(__FILE__, __LINE__, [&]() {
         cudaFree(mstruct.fermi_dev_p);
         cudaFree(mstruct.barrier_dev_p);
-        cudaFree(mstruct.bandgap_dev_p);
-        cudaFree(mstruct.phononloss_dev_p);
+        cudaFree(mstruct.band_gap_dev_p);
+        cudaFree(mstruct.band_edge_dev_p);
+        cudaFree(mstruct.effective_mass_dev_p);
+        cudaFree(mstruct.phonon_loss_dev_p);
         cudaFree(mstruct.elastic_dev_p);
         cudaFree(mstruct.inelastic_dev_p);
         cudaFree(mstruct.ionization_dev_p);
@@ -49,14 +53,32 @@ __host__ void cuda_material_struct::assign(int i, const material& _material) {
         cuda_mem_scope<float>(barrier_dev_p, capacity, [&](float* barrier_p) {
             barrier_p[i] = _material.barrier()/constant::ec;
         });
-        cuda_mem_scope<float>(bandgap_dev_p, capacity, [&](float* bandgap_p) {
-            if(_material.bandgap().is_defined())
-                bandgap_p[i] = _material.bandgap()()/constant::ec;
+        cuda_mem_scope<float>(band_gap_dev_p, capacity, [&](float* band_gap_p) {
+            if(_material.band_gap().is_defined())
+                band_gap_p[i] = _material.band_gap()()/constant::ec;
             else
-                bandgap_p[i] = -1;
+                band_gap_p[i] = -1;
         });
-        cuda_mem_scope<float>(phononloss_dev_p, capacity, [&](float* phononloss_p) {
-            phononloss_p[i] = _material.phononloss()/constant::ec;
+        cuda_mem_scope<float>(band_edge_dev_p, capacity, [&](float* band_edge_p) {
+            #warning "band edge is determined for silicon and pmma by using string comparison"
+            if(_material.name() == "silicon" || _material.name() == "silicon-surface")
+                band_edge_p[i] = _material.barrier()/constant::ec-4.05f;
+            else if(_material.name() == "pmma" || _material.name() == "pmma-surface")
+                band_edge_p[i] = _material.barrier()/constant::ec-2.5f;
+            else
+                band_edge_p[i] = 0;
+        });
+        cuda_mem_scope<float>(effective_mass_dev_p, capacity, [&](float* effective_mass_p) {
+            #warning "effective mass is determined for silicon and pmma by using string comparison"
+            if(_material.name() == "silicon" || _material.name() == "silicon-surface")
+                effective_mass_p[i] = 1.09f;
+            else if(_material.name() == "pmma" || _material.name() == "pmma-surface")
+                effective_mass_p[i] = 1.0f;
+            else
+                effective_mass_p[i] = 1.0f;
+        });
+        cuda_mem_scope<float>(phonon_loss_dev_p, capacity, [&](float* phonon_loss_p) {
+            phonon_loss_p[i] = _material.phonon_loss()/constant::ec;
         });
     });
     auto __logspace_K_at = [&](int x) {
@@ -87,8 +109,8 @@ __host__ void cuda_material_struct::assign(int i, const material& _material) {
                 const double P =  __linspace_P_at(y);
                 for(int x = 0; x < K_cnt; x++) {
                     const double K = __logspace_K_at(x)*constant::ec;
-                    elastic_icdf_p[y][x] = std::cos(_material.elastic_icdf(K, P));
-                    inelastic_icdf_p[y][x] = std::log(_material.inelastic_icdf(K, P)/constant::ec);
+                    elastic_icdf_p[y][x] = std::cos(max(0.0, min(M_PI, _material.elastic_icdf(K, P))));
+                    inelastic_icdf_p[y][x] = std::log(max(0.0, min((K-_material.fermi())/constant::ec, _material.inelastic_icdf(K, P)/constant::ec)));
                 }
             }
         });

src/cdsem/cuda_material_struct.cuh
@@ -17,7 +17,7 @@ public:
 
     __host__ void assign(int i, const material&);
 
-    const float K_min = 1e-3f;
+    const float K_min = 1.0f;
     const float K_max = 10e3f;
     const int K_cnt = 1024;
     const int P_cnt = 1024;
@@ -26,8 +26,10 @@ public:
     int pitch;
     float* fermi_dev_p;
     float* barrier_dev_p;
-    float* bandgap_dev_p;
-    float* phononloss_dev_p;
+    float* band_gap_dev_p;
+    float* band_edge_dev_p;
+    float* effective_mass_dev_p;
+    float* phonon_loss_dev_p;
     float* elastic_dev_p;
     float* inelastic_dev_p;
     float* ionization_dev_p;

commit 0cc1d18d98cb684a04b3792be6fdfe30a606422d

Fix error in interpolation between phonon regimes.
For E < E_BZ/4 or E > E_BZ, only the values at E_BZ/4 and E_BZ were used.
This is a regression introduced in 29256444fb40b8d3b8a533bdaebe3997a2a56d75.

scripts/phonon-gen.py
@@ -22,48 +22,19 @@ def compose(*f):
 
 def interpolate(f1, f2, h, a, b):
     """Interpolate two functions `f1` and `f2` using interpolation
-    function `h`, which maps [0,1] to [0,1] one-to-one."""
+    function `h`, which maps [0,1] to [0,1] one-to-one.
+    If x is outside the requested interpolation regime, returns the
+    relevant function"""
     def g(x):
-        y1 = f1(a)
-        y2 = f2(b)
         u = (x - a) / (b - a)
         w = h(u)
-        ym = (1 - w) * y1 + w * y2
+        ym = (1 - w) * f1(a) + w * f2(b)
 
         return np.where(
-            x < a, y1, np.where(
-                x > b, y2, ym))
+            x < a, f1(x), np.where(
+                x > b, f2(x), ym))
 
     return g
-
-
-def linear_interpolate(f1, f2, h, a, b):
-    ya = f1(a)
-    yb = f2(b)
-
-    def fm(x):
-        n = h((x - a) / (b - a))
-        return (1 - n) * ya + n * yb
-
-    def g(x):
-        y1 = f1(x)
-        y2 = f2(x)
-        ym = fm(x)
-
-        return np.where(
-            x < a, y1, np.where(
-                x > b, y2, ym))
-
-    return g
-
-
-def log_interpolate(f1, f2, h, a, b):
-    """Interpolate two functions `f1` and `f2` using interpolation
-    function `h`, which maps [0,1] to [0,1] one-to-one."""
-    f1p = compose(log, f1, exp)
-    f2p = compose(log, f2, exp)
-    g = linear_interpolate(f1p, f2p, h, log(a), log(b))
-    return compose(exp, g, log)
 
 
 if __name__ == "__main__":